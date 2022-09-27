e4c309a1bd8dbcc39ed9275851d800de869b65fe
Duyet Le <lvduyet@fossil.com>
2022-09-28 00:05:44 +0700

    feat: GA4 via Partytown

---
+++ src/components/root-element.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react'
+import { Script, graphql, useStaticQuery } from 'gatsby'
+
+export const useSiteMetadata = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          measurementId
+        }
+      }
+    }
+  `)
+
+  return data.site.siteMetadata
+}
+
+const RootElement = ({ children }) => {
+  const { measurementId } = useSiteMetadata()
+
+  return (
+    <Fragment>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy='off-main-thread'
+        forward={[`gtag`]}
+      />
+      <Script
+        id='gtag-config'
+        strategy='off-main-thread'
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          window.gtag = function gtag(){ window.dataLayer.push(arguments);}
+          gtag('js', new Date()); 
+          gtag('config', '${measurementId}', { send_page_view: false })`,
+        }}
+      />
+      <div>{children}</div>
+    </Fragment>
+  )
+}
+
+export default RootElement
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   AboutSection,
   ArticlesSection,
@@ -8,20 +8,24 @@ import {
   Page,
   ProjectsSection,
   Seo,
-} from "gatsby-theme-portfolio-minimal";
+} from 'gatsby-theme-portfolio-minimal'
 
 export default function IndexPage() {
   return (
     <>
-      <Seo title="my /maɪ/ personal blog" />
+      <Seo title='my /maɪ/ personal blog' />
       <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Blog']} />
-        <AboutSection sectionId="about" heading="About me" />
-        <InterestsSection sectionId="details" heading="Interest" />
-        <ProjectsSection sectionId="topic" heading="Topic" />
-        <ContactSection sectionId="contact" heading="Let's Keep In Touch" />
+        <HeroSection sectionId='hero' />
+        <ArticlesSection
+          sectionId='articles'
+          heading='Latest Articles'
+          sources={['Blog']}
+        />
+        <AboutSection sectionId='about' heading='About me' />
+        <InterestsSection sectionId='details' heading='Interest' />
+        <ProjectsSection sectionId='topic' heading='Topic' />
+        <ContactSection sectionId='contact' heading="Let's Keep In Touch" />
       </Page>
     </>
-  );
+  )
 }
